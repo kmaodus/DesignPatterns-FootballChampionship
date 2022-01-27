ed2bdb04bf44e669f6b5073619a54e4902a8d296
Kristijan Maoduš <kmaodus@foi.hr>
2022-01-27 04:33:28 +0100

    zadaca 2 predana!

--- kmaodus_zadaca_2/kmaodus_zadaca_2/kmaodus_zadaca_2/Entiteti/Sastav.cs
+++ kmaodus_zadaca_2/kmaodus_zadaca_2/kmaodus_zadaca_2/Entiteti/Sastav.cs
@@ -6,7 +6,6 @@ namespace kmaodus_zadaca_2.Entiteti
     {
         public Klub Klub { get; set; }
 
-        // provjeriti da igrac ima samo jednu poziciju u sastavu, od pozicija koje su mu ucitane
         public List<Igrac> Igraci { get; set; }
 
         public Sastav()
